3f3c180e469fa9c3f9e9dce4b1cab332ed855323
(Jason)Xinbo Chen <chenxinbo@outlook.com>
2019-03-24 12:51:28 -0400

    Update companysJobs.php

--- companysJobs.php
+++ companysJobs.php
@@ -15,6 +15,7 @@
 	?>
 	<form action="companysJobs.php" method="post">
 		<select name = "company_name" method = "post">
+			<option>All Available Jobs</option>
 			<?php
 				$sql = "select distinct company_name from Job_Post";
 				$stmt = $pdo->prepare($sql);   #create the query
@@ -31,14 +32,26 @@
 		if(isset($_POST['submit'])){
 			$result = isset($_POST['company_name'])?$_POST['company_name']:'';
 			
+			if ($result == 'All Available Jobs'){	
+				echo "<h2>$result Job List</h2>";
+				echo "<table><tr><th>job_id</th><th>title</th><th>city</th><th>province</th><th>pay_rate</th><th>company_name</th></tr>";
+				$sql = "select job_id, title, city, province, pay_rate, company_name from Job_Post";
+				$stmt = $pdo->prepare($sql);
+				$stmt -> execute([$result]);
+				while ($row = $stmt->fetch()) {
+					echo "<tr><td>".$row["job_id"]."</td><td>".$row["title"]."</td><td>".$row["city"]."</td><td>".$row["province"]."</td><td>".$row["pay_rate"]."</td><td>".$row["company_name"]."</td></tr>";
+				}
+			}
 			
-			echo "<p>$result Job List</h2></p>";
-			echo "<table><tr><th>job_id</th><th>title</th><th>city</th><th>province</th><th>pay_rate</th><th>company_name</th></tr>";
-			$sql = "select job_id, title, city, province, pay_rate, company_name from Job_Post where company_name = ?";
-			$stmt = $pdo->prepare($sql);
-			$stmt -> execute([$result]);
-			while ($row = $stmt->fetch()) {
-				echo "<tr><td>".$row["job_id"]."</td><td>".$row["title"]."</td><td>".$row["city"]."</td><td>".$row["province"]."</td><td>".$row["pay_rate"]."</td><td>".$row["company_name"]."</td></tr>";
+			else{
+				echo "<p>$result Job List</p>";
+				echo "<table><tr><th>job_id</th><th>title</th><th>city</th><th>province</th><th>pay_rate</th><th>company_name</th></tr>";
+				$sql = "select job_id, title, city, province, pay_rate, company_name from Job_Post where company_name = ?";
+				$stmt = $pdo->prepare($sql);
+				$stmt -> execute([$result]);
+				while ($row = $stmt->fetch()) {
+					echo "<tr><td>".$row["job_id"]."</td><td>".$row["title"]."</td><td>".$row["city"]."</td><td>".$row["province"]."</td><td>".$row["pay_rate"]."</td><td>".$row["company_name"]."</td></tr>";
+				}
 			}
 		}
 	?>
