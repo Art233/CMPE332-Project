a8854f4b10da0a31f391177f188e3ef4115ae204
Art233 <40292037+Art233@users.noreply.github.com>
2019-03-24 15:31:14 -0400

    Add files via upload

---
+++ Connection.php
@@ -0,0 +1,8 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=332project'; //Data source Name
+$username = 'root';
+$password = '';
+//$options = array(PDO::MYSQL_ATTR_INIT_COMMAND=> 'SET NAMES utf8');
+
+ $pdo = new PDO($dsn, $username, $password);
+ ?>
