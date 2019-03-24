dd0feba33e00c8111cd91c13c5955d2e0cd95211
(Jason)Xinbo Chen <chenxinbo@outlook.com>
2019-03-24 12:59:13 -0400

    Add files via upload

---
+++ attendeesList.php
@@ -0,0 +1,44 @@
+<?php
+		require 'connection.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Attendees List</title>
+<link href="web.css" type="text/css" rel="stylesheet" >
+</head>
+<body>
+	<?php
+		$pdo = new PDO('mysql:host=localhost;dbname=332project', "root", "");
+	?>
+	<?php	
+		echo "<p><h1>Students List</h1></p>";
+		echo "<table><tr><th>ID</th><th>first_name</th><th>last_name</th><th>attendee_type</th></tr>";
+		$sql = "select ID, first_name, last_name, attendee_type, from Attendees where attendee_type = Student";
+		$stmt = $pdo->prepare($sql);
+		$stmt -> execute();
+		while ($row = $stmt->fetch()) {
+			echo "<tr><td>".$row["ID"]."</td><td>".$row["first_name"]."</td><td>".$row["last_name"]."</td><td>".$row["attendee_type"]."</td></tr>";
+		}
+		
+		echo "<p><h1>Professionals List</h1></p>";
+		echo "<table><tr><th>ID</th><th>first_name</th><th>last_name</th><th>attendee_type</th></tr>";
+		$sql = "select ID, first_name, last_name, attendee_type, from Attendees where attendee_type = Professional";
+		$stmt = $pdo->prepare($sql);
+		$stmt -> execute();
+		while ($row = $stmt->fetch()) {
+			echo "<tr><td>".$row["ID"]."</td><td>".$row["first_name"]."</td><td>".$row["last_name"]."</td><td>".$row["attendee_type"]."</td></tr>";
+		}
+		
+		echo "<p><h1>Sponsors List</h1></p>";
+		echo "<table><tr><th>ID</th><th>first_name</th><th>last_name</th><th>attendee_type</th></tr>";
+		$sql = "select ID, first_name, last_name, attendee_type, from Attendees where attendee_type = Sponsor";
+		$stmt = $pdo->prepare($sql);
+		$stmt -> execute();
+		while ($row = $stmt->fetch()) {
+			echo "<tr><td>".$row["ID"]."</td><td>".$row["first_name"]."</td><td>".$row["last_name"]."</td><td>".$row["attendee_type"]."</td></tr>";
+		}
+	?>
+
+</body>
+</html>
