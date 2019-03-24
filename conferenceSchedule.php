6a04fb01a17d163b741ed726f8eedf32dbc1fba7
(Jason)Xinbo Chen <chenxinbo@outlook.com>
2019-03-24 11:59:52 -0400

    Update conferenceSchedule.php

--- conferenceSchedule.php
+++ conferenceSchedule.php
@@ -12,7 +12,7 @@
 	?>
 	<form action="conferenceSchedule.php" method="post">
 		<select name = "date" method = "post">
-			<option>All Date</option>
+			#<option>All Date</option>
 			<?php
 				$sql = "select distinct date from Conference_Schedule";
 				$stmt = $pdo->prepare($sql);   #create the query
@@ -28,25 +28,15 @@
 	<?php
 		if(isset($_POST['submit'])){
 			$result = isset($_POST['date'])?$_POST['date']:'';
-			if ($result == 'All Date'){	#???????
-				echo "<h2>$result Conference Schedule</h2>";
-				echo "<table><tr><th>section_name</th><th>section_id</th><th>start_time</th><th>end_time</th><th>speaker_front_name</th><th>speaker_last_name</th></tr>";
-				$sql = "select section_name, section_id, start_time, end_time, speaker_front_name, speaker_last_name from Conference_Schedule where date = date"; #???????
-				$stmt = $pdo->prepare($sql);
-				$stmt -> execute();
-				while ($row = $stmt->fetch()) {
-					echo "<tr><td>".$row["section_name"]."</td><td>".$row["section_id"]."</td><td>".$row["start_time"]."</td><td>".$row["end_time"]."</td><td>".$row["speaker_front_name"]."</td><td>".$row["speaker_last_name"]."</td></tr>";
-				}
-			}
-			else{#might not need
-				echo "<p>$result Member List</p>";
-				echo "<table><tr><th>First Name</th><th>Last Name</th></tr>";
-				$sql = "select first_name, last_name,room_number from students where room_number = ?";
-				$stmt = $pdo->prepare($sql);
-				$stmt -> execute([$result]);
-				while ($row = $stmt->fetch()) {
-					echo "<tr><td>".$row["first_name"]."</td><td>".$row["last_name"]."</td></tr>";
-			}
+			
+			
+			echo "<p>$result Conference Schedule</h2></p>";
+			echo "<table><tr><th>section_name</th><th>section_id</th><th>start_time</th><th>end_time</th><th>speaker_front_name</th><th>speaker_last_name</th></tr>";
+			$sql = "select section_name, section_id, start_time, end_time, speaker_front_name, speaker_last_name from Conference_Schedule where date = ?";
+			$stmt = $pdo->prepare($sql);
+			$stmt -> execute([$result]);
+			while ($row = $stmt->fetch()) {
+				echo "<tr><td>".$row["section_name"]."</td><td>".$row["section_id"]."</td><td>".$row["start_time"]."</td><td>".$row["end_time"]."</td><td>".$row["speaker_front_name"]."</td><td>".$row["speaker_last_name"]."</td></tr>";
 			}
 		}
 	?>
